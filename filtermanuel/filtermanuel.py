f372aecedb9e3ef1a901b646867a2147ca142c66
Matt Giltaji <matt@giltaji.com>
2016-05-31 00:23:21 -0700

    initial commit of filtermanuel util

---
+++ filtermanuel/filtermanuel.py
@@ -0,0 +1,7 @@
+# This script is intended to filter a file by lines contained in another
+# This is as trivial as it sounds; we need to maintain insertion order
+# More practically (in terms of Kingdom of Loathing):
+#   Take the output from a monster manuel status utility
+#   Find the matching monsters in a faxbot list
+#   Output the list of matches (in manuel order) into an output file
+#   We would also like to preserve area headings from manuel in the output
