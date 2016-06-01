f1b05830b6013a1ca78dc80b01f9f6d7d3f39d63
Matt Giltaji <matt@giltaji.com>
2016-06-01 00:31:30 -0700

    initial py.test (sorta works :/)

---
+++ tests/test_filtermanuel.py
@@ -0,0 +1,10 @@
+# Tests for filtermanuel.py
+# run from miscutils dir as:
+#    py.test tests/test_filtermanuel.py
+
+import pytest
+# import filtermanuel as fm
+
+
+def test_should_copy():
+    assert True
