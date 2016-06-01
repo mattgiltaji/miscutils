359909d9484eb453b39ea49437fb40745be42d35
Matt Giltaji <matt@giltaji.com>
2016-06-01 01:11:00 -0700

    should_copy identifies separators

--- filtermanuel/filtermanuel.py
+++ filtermanuel/filtermanuel.py
@@ -5,3 +5,25 @@
 #   Find the matching monsters in a faxbot list
 #   Output the list of matches (in manuel order) into an output file
 #   We would also like to preserve area headings from manuel in the output
+
+import re
+
+SECTION_SEPARATOR_REGEX = '^=====*$'
+# this regex should match the section separator.
+# Why 5 equal signs? entirely arbitrary
+
+
+def should_copy(contents):
+    """ Returns true if contents should be copied to output file
+
+    3 types of lines should return true:
+    A) a monster that matches a line in the faxbot list
+    B) a section header - [foo bar]
+    C) a section divider - =====...===
+    We don't necessarily evaluate these in order, we try the easy ones first
+
+    :param str contents: data that will be checked
+    :return: Boolean indicating the line should be copied to output
+    """
+
+    return re.match(SECTION_SEPARATOR_REGEX, contents)
