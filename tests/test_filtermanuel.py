3517932c10760a20deb1f702686e78fbbd374baa
Matt Giltaji <matt@giltaji.com>
2016-06-01 01:11:00 -0700

    should_copy identifies separators

--- tests/test_filtermanuel.py
+++ tests/test_filtermanuel.py
@@ -3,8 +3,20 @@
 #    py.test tests/test_filtermanuel.py
 
 import pytest
-import filtermanuel as fm
+import filtermanuel.filtermanuel as fm
 
 
-def test_should_copy():
-    assert True
+class TestShouldCopy:
+
+    def test_should_copy_separator(self):
+        assert fm.should_copy("=================================")  # real line
+        assert fm.should_copy("==========")  # arbitrarily short but valid
+        assert fm.should_copy("====")  # shortest possible valid
+        assert not fm.should_copy("=")
+        assert not fm.should_copy("==")
+        assert not fm.should_copy("===")  # longest invalid
+
+        assert not fm.should_copy("---------------------------")  # wrong symbol
+        assert not fm.should_copy("-=-=-=-=-=-=-=-=-=-=")  # no mixing
+        assert not fm.should_copy("----=======-----")  # nope
+        assert not fm.should_copy("============================-")  # no dash
