ebfd90d1caac60d5c7a0e536ad5dab2cd4ecb3a8
Matt Giltaji <matt@giltaji.com>
2016-07-17 12:10:58 -0700

    refactor to use test parameterization

--- tests/test_filtermanuel.py
+++ tests/test_filtermanuel.py
@@ -5,62 +5,63 @@
 import pytest
 import filtermanuel.filtermanuel as fm
 
+# hardcoding fun
+fake_monster_file_contents = ("Monster", "Monster'1", "Monster 2", "Monster.37",
+                              "monster-dash", "comma, the monster")
+
 
 class TestShouldCopy:
+    @pytest.mark.parametrize("test_input", [
+        "=================================",  # real line
+        "=====================================",  # longer
+        "==========",# arbitrarily short but valid
+        "====",  # shortest possible valid
+    ])
+    def test_copy_separator(self, test_input):
+        assert fm.should_copy(test_input)
 
-    def test_copy_separator(self):
-        assert fm.should_copy("=================================")  # real line
-        assert fm.should_copy("=====================================")  # longer
-        assert fm.should_copy("==========")  # arbitrarily short but valid
-        assert fm.should_copy("====")  # shortest possible valid
+    @pytest.mark.parametrize("test_input", [
+        "=", "==", "===",  # all too short
+        "---------------------------",  # wrong symbol
+        "-=-=-=-=-=-=-=-=-=-=",  # no mixing
+        "----=======-----",  # nope
+        "============================-",  # no dash
+    ])
+    def test_dont_copy_bad_separator(self,test_input):
+        assert not fm.should_copy(test_input)
 
-    def test_dont_copy_bad_separator(self):
-        assert not fm.should_copy("=")
-        assert not fm.should_copy("==")
-        assert not fm.should_copy("===")  # longest invalid
-
-        assert not fm.should_copy("---------------------------")  # wrong symbol
-        assert not fm.should_copy("-=-=-=-=-=-=-=-=-=-=")  # no mixing
-        assert not fm.should_copy("----=======-----")  # nope
-        assert not fm.should_copy("============================-")  # no dash
-
-    def test_copy_area_name(self):
-        # real area
-        assert fm.should_copy("[Ye Olde Medievale Villagee]")
+    @pytest.mark.parametrize("test_input", [
+        "[Ye Olde Medievale Villagee]",  # real area
         # real areas (with fun characters)
-        assert fm.should_copy("[An Incredibly Strange Place (Mediocre Trip)]")
-        assert fm.should_copy("[Anger Man's Level]")
-        assert fm.should_copy("[The Gourd!]")
-        assert fm.should_copy("[LavaCo™ Lamp Factory]")
-        assert fm.should_copy("[A Deserted Stretch of I-911]")
+        "[An Incredibly Strange Place (Mediocre Trip)]",
+        "[Anger Man's Level]",
+        "[The Gourd!]",
+        "[LavaCo™ Lamp Factory]",
+        "[A Deserted Stretch of I-911]",
+    ])
+    def test_copy_area_name(self, test_input):
+        assert fm.should_copy(test_input)
 
-    def test_dont_copy_bad_area_name(self):
-        assert not fm.should_copy("[]")
-        assert not fm.should_copy("[ ]")
+    @pytest.mark.parametrize("test_input", [
+        "[]", "[ ]",  # no blanks
         # fake areas to test regex bounds
-        assert not fm.should_copy("[a]")
-        assert not fm.should_copy("[urmom]")
-        assert not fm.should_copy("[Urmom]")
-        assert not fm.should_copy("[aarht shgthde aetgfgd]")
+        "[a]", "[urmom]", "[Urmom]", "[aarht shgthde aetgfgd]"
+    ])
+    def test_dont_copy_bad_area_name(self, test_input):
+        assert not fm.should_copy(test_input)
 
-    def test_copy_matching_monster(self):
-        file_contents = ("Monster", "Monster'1", "Monster 2", "Monster.37",
-                         "monster-dash", "comma, the monster")
-        assert fm.should_copy("Monster", file_contents)
-        assert fm.should_copy("Monster 2", file_contents)
-        assert fm.should_copy("Monster'1", file_contents)
-        assert fm.should_copy("comma, the monster", file_contents)
-        assert fm.should_copy("monster-dash", file_contents)
-        assert fm.should_copy("Monster.37", file_contents)
+    @pytest.mark.parametrize("test_input", [
+        "Monster", "Monster 2", "Monster'1", "comma, the monster",
+        "monster-dash", "Monster.37",
+    ])
+    def test_copy_matching_monster(self, test_input):
+        assert fm.should_copy(test_input, fake_monster_file_contents)
 
-    def test_dont_copy_nonmatching_monster(self):
-        file_contents = ("Monster", "Monster'1", "Monster 2", "Monster.37",
-                         "monster-dash", "comma, the monster")
-        assert not fm.should_copy("monster", file_contents)
-        assert not fm.should_copy("yolo", file_contents)
-        assert not fm.should_copy("comma, ", file_contents)
-        assert not fm.should_copy("-dash", file_contents)
-        assert not fm.should_copy("37", file_contents)
+    @pytest.mark.parametrize("test_input", [
+        "monster", "yolo", "comma, ", "-dash", "37",
+    ])
+    def test_dont_copy_nonmatching_monster(self, test_input):
+        assert not fm.should_copy(test_input, fake_monster_file_contents)
 
 
 class TestGetFileContents:
