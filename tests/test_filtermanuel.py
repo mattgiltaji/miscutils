473a3e077b20457bf9883087908e596287e9cf2b
Matt Giltaji <matt@giltaji.com>
2016-09-12 01:23:25 -0700

    test setups for reading files

--- tests/test_filtermanuel.py
+++ tests/test_filtermanuel.py
@@ -2,8 +2,20 @@
 # run from miscutils dir as:
 #    py.test tests/test_filtermanuel.py
 
+from os import path
 import pytest
+
 import filtermanuel.filtermanuel as fm
+
+# paths to test files and such
+current_dir = path.dirname(path.abspath(__file__))
+test_data_dir = path.join(current_dir, 'files', 'filtermanuel')
+basic_test_dir = path.join(test_data_dir, 'basic_files')
+small_file = path.join(basic_test_dir, 'small.txt')
+blank_file = path.join(basic_test_dir, 'blank.txt')
+big_file = path.join(basic_test_dir, 'big.txt')
+not_exist_file = path.join(basic_test_dir, 'not_exist.txt')
+
 
 # hardcoding fun
 fake_monster_file_contents = ("Monster", "Monster'1", "Monster 2", "Monster.37",
@@ -66,13 +78,17 @@ class TestShouldCopy:
 
 class TestGetFileContents:
     def test_get_blank_file_contents(self):
-        pass
+        results = fm.get_file_contents(blank_file)
+        assert results == []
 
     def test_error_on_bad_filename(self):
-        pass
+        with pytest.raises(FileNotFoundError) as excinfo:
+            fm.get_file_contents(not_exist_file)
 
     def test_get_big_file_contents(self):
-        pass
+        results = fm.get_file_contents(big_file)
+        assert results.length == 5001
 
     def test_get_small_file_contents(self):
-        pass
+        results = fm.get_file_contents(small_file)
+        assert results.length == 11
