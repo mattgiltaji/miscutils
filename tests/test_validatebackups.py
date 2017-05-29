939e70fc73cf901dd247e8b029ca039a07d21ee3
Matt Giltaji <matt@giltaji.com>
2017-05-29 14:41:59 -0700

    adding tests and utility functions

--- tests/test_validatebackups.py
+++ tests/test_validatebackups.py
@@ -1,3 +1,29 @@
 # Tests for validatebackups.py
 # run from miscutils dir as:
 #    python -m pytest tests/test_validatebackups.py
+import os
+import pytest
+
+import validatebackups.validatebackups as vb
+from google.cloud import storage
+
+
+@pytest.fixture()
+def gcs_client():
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = r"D:\Matt\Documents\google cloud storage\test-backup-validator-auth.json"
+    client = storage.Client()
+    yield client
+
+class TestGetOldestBlob:
+
+    def test_get_oldest_blob(self, gcs_client):
+        bucket = gcs_client.get_bucket("test-matt-server-backups")
+        result = vb.get_oldest_blob(bucket.list_blobs())
+        assert result.name == "oldest.txt"
+
+class TestGetNewestBlob:
+
+    def test_get_newest_blob(self, gcs_client):
+        bucket = gcs_client.get_bucket("test-matt-server-backups")
+        result = vb.get_newest_blob(bucket.list_blobs())
+        assert result.name == "newest.txt"
--- validatebackups/validatebackups.py
+++ validatebackups/validatebackups.py
@@ -6,17 +6,68 @@
 # These validation procedures are similar but not identical
 # Report success/failure for each bucket
 import argparse
+import os
 import sys
 from google.cloud import storage
 
 
+def get_oldest_blob(blobs):
+    oldest = None
+    for blob in blobs:
+        if oldest is None:
+            oldest = blob
+        if blob.time_created < oldest.time_created:
+            oldest = blob
+    return oldest
+
+
+def get_newest_blob(blobs):
+    newest = None
+    for blob in blobs:
+        if newest is None:
+            newest = blob
+        if blob.time_created > newest.time_created:
+            newest = blob
+    return newest
+
+
+def validate_giltaji_media_bucket(bucket):
+    pass
+
+
+def validate_giltaji_photos_bucket(bucket):
+    pass
+
+
+def validate_matt_server_backups_bucket(bucket):
+    # check timestamps on most recent, should be within 8 days of today
+    # check timestamps on oldest, should be within 2 months of today
+    for blob in bucket.list_blobs(fields="items/name,items/time_created"):
+        pass
+
+    # download most recent 4
+
+
+
+def validate_bucket(bucket):
+    if bucket.name == "giltaji-media":
+        validate_giltaji_media_bucket(bucket)
+    elif bucket.name == "giltaji-photos":
+        validate_giltaji_photos_bucket(bucket)
+    elif bucket.name == "matt-server-backups":
+        validate_matt_server_backups_bucket(bucket)
+    elif bucket.name == "giltajidjangobackups":
+        pass
+    else:
+        raise ValueError("Unable to handle Bucket " + bucket.name)
+
+
 def validate_backups():
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = r"D:\Matt\Documents\google cloud storage\backup-validator-auth.json"
     client = storage.Client()
 
     for bucket in client.list_buckets():
-        print(bucket)
-
-    # send to appropriate subfunction to handle it
+        validate_bucket(bucket)
 
 
 def parse_args(args):
@@ -26,7 +77,7 @@ def parse_args(args):
         :return: arguments parsed into a neat object
         """
     parser = argparse.ArgumentParser(
-        description='Download random sample of backup files for validation')
+        description="Download random sample of backup files for validation")
     return parser.parse_args(args)
 
 
