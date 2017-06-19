29177ba86a1c296c26818dc880d14ddbe563076c
Matt Giltaji <matt@giltaji.com>
2017-06-18 22:00:26 -0700

    refactor to use a class instead of passing buckets all over

--- tests/test_validatebackups.py
+++ tests/test_validatebackups.py
@@ -4,45 +4,47 @@
 import os
 import pytest
 
-import validatebackups.validatebackups as vb
+from validatebackups.validatebackups import BackupValidator
 from google.cloud import storage
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 test_data_dir = os.path.join(current_dir, "files", "validatebackups")
 matt_server_backups_dir = os.path.join(test_data_dir, "matt-server-backups")
-matt_server_backups_download_dir = os.path.join(vb.FILE_DOWNLOAD_LOCATION, "test-matt-server-backups-fresh")
+matt_server_backups_download_dir = os.path.join(BackupValidator.FILE_DOWNLOAD_LOCATION, "test-matt-server-backups-fresh")
 
 
 @pytest.fixture
-def gcs_client():
+def validator():
     os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = \
         r"D:\Matt\Documents\google cloud storage\test-backup-validator-auth.json"
-    client = storage.Client()
-    yield client
+    backup_validator = BackupValidator(client=storage.Client())
+    yield backup_validator
 
 
 # noinspection PyShadowingNames
 @pytest.fixture
-def test_backup_bucket(gcs_client):
-    yield gcs_client.get_bucket("test-matt-server-backups")
+def test_backup_validator(validator):
+    validator.server_backups_bucket = validator.client.get_bucket("test-matt-server-backups")
+    yield validator
 
 
 # noinspection PyShadowingNames
 @pytest.fixture
-def old_backup_bucket(gcs_client):
-    yield gcs_client.get_bucket("test-matt-server-backups-old")
+def old_backup_validator(validator):
+    validator.server_backups_bucket = validator.client.get_bucket("test-matt-server-backups-old")
+    yield validator
 
 
 # noinspection PyShadowingNames
 @pytest.fixture
-def fresh_backup_bucket_with_uploads(gcs_client):
-    bucket = gcs_client.get_bucket("test-matt-server-backups-fresh")
-    delete_existing_blobs_from_bucket(bucket)
+def fresh_backup_validator_with_uploads(validator):
+    validator.server_backups_bucket = validator.client.get_bucket("test-matt-server-backups-fresh")
+    delete_existing_blobs_from_bucket(validator.server_backups_bucket)
     upload_new_file_to_bucket(
-        bucket=bucket,
+        bucket=validator.server_backups_bucket,
         file_location=os.path.join(matt_server_backups_dir, "newest.txt")
     )
-    yield bucket
+    yield validator
 
 
 def delete_existing_blobs_from_bucket(bucket):
@@ -58,17 +60,17 @@ def upload_new_file_to_bucket(bucket, file_location):
 
 # noinspection PyShadowingNames
 @pytest.fixture
-def fresh_backup_bucket_with_downloads(gcs_client):
-    bucket = gcs_client.get_bucket("test-matt-server-backups-fresh")
-    delete_existing_blobs_from_bucket(bucket)
+def fresh_backup_validator_with_downloads(validator):
+    validator.server_backups_bucket = validator.client.get_bucket("test-matt-server-backups-fresh")
+    delete_existing_blobs_from_bucket(validator.server_backups_bucket)
     delete_existing_files_from_directory(matt_server_backups_download_dir)
-    for number in range(1, vb.NUM_SERVER_BACKUPS_TO_DOWNLOAD + 1):
+    for number in range(1, BackupValidator.NUM_SERVER_BACKUPS_TO_DOWNLOAD + 1):
         upload_new_file_to_bucket(
-            bucket=bucket,
+            bucket=validator.server_backups_bucket,
             file_location=os.path.join(matt_server_backups_dir, "mid" + str(number) + ".txt")
         )
 
-    yield bucket
+    yield validator
 
 
 def delete_existing_files_from_directory(directory_location):
@@ -87,51 +89,59 @@ class TestPhotosBucket:
 # noinspection PyShadowingNames
 class TestBlobSorting:
 
-    def test_get_oldest_blob(self, test_backup_bucket):
-        result = vb.get_oldest_blob(test_backup_bucket.list_blobs())
+    def test_get_oldest_blob(self, test_backup_validator):
+        result = test_backup_validator.get_oldest_blob(
+            test_backup_validator.server_backups_bucket.list_blobs()
+        )
         assert result.name == "oldest.txt"
 
-    def test_get_newest_blob(self, test_backup_bucket):
-        result = vb.get_newest_blob(test_backup_bucket.list_blobs())
+    def test_get_newest_blob(self, test_backup_validator):
+        result = test_backup_validator.get_newest_blob(
+            test_backup_validator.server_backups_bucket.list_blobs()
+        )
         assert result.name == "newest.txt"
 
-    def test_get_blobs_sorted_newest_to_oldest(self, test_backup_bucket):
+    def test_get_blobs_sorted_newest_to_oldest(self, test_backup_validator):
         expected = ["newest.txt", "new2.txt", "new3.txt", "new4.txt",
                     "mid6.txt", "mid5.txt", "mid4.txt", "mid3.txt", "mid2.txt", "mid1.txt",
                     "oldest.txt"]
-        results = vb.get_blobs_sorted_newest_to_oldest(test_backup_bucket.list_blobs())
+        results = test_backup_validator.get_blobs_sorted_newest_to_oldest(
+            test_backup_validator.server_backups_bucket.list_blobs()
+        )
         actual = list(result.name for result in results)
         assert expected == actual
 
-    def test_get_most_recent_blobs_to_download(self, test_backup_bucket):
+    def test_get_most_recent_blobs_to_download(self, test_backup_validator):
         expected = ["newest.txt", "new2.txt", "new3.txt", "new4.txt"]
-        results = vb.get_most_recent_blobs_to_download(test_backup_bucket.list_blobs())
+        results = test_backup_validator.get_most_recent_backup_blobs_to_download(
+            test_backup_validator.server_backups_bucket.list_blobs()
+        )
         actual = list(result.name for result in results)
         assert expected == actual
 
 
 # noinspection PyShadowingNames
 class TestValidateMattServerBackupsBucket:
-    def test_validate_newest_file_throws_warnings_properly(self, old_backup_bucket):
+    def test_validate_newest_file_throws_warnings_properly(self, old_backup_validator):
         with pytest.warns(None) as record:
-            vb.validate_newest_file_in_proper_age_range(bucket=old_backup_bucket)
+            old_backup_validator.validate_newest_file_in_proper_age_range()
         assert len(record) > 0
 
     @pytest.mark.skip(reason="file not old enough- try after 7/29")
-    def test_validate_oldest_file_throws_warnings_properly(self, old_backup_bucket):
+    def test_validate_oldest_file_throws_warnings_properly(self, old_backup_validator):
         with pytest.warns(None) as record:
-            vb.validate_oldest_file_in_proper_age_range(bucket=old_backup_bucket)
+            old_backup_validator.validate_oldest_file_in_proper_age_range()
         assert len(record) > 0
 
-    def test_validate_fresh_files_has_no_warnings(self, fresh_backup_bucket_with_uploads):
+    def test_validate_fresh_files_has_no_warnings(self, fresh_backup_validator_with_uploads):
         with pytest.warns(None) as record:
-            vb.validate_newest_file_in_proper_age_range(bucket=fresh_backup_bucket_with_uploads)
-            vb.validate_oldest_file_in_proper_age_range(bucket=fresh_backup_bucket_with_uploads)
+            fresh_backup_validator_with_uploads.validate_newest_file_in_proper_age_range()
+            fresh_backup_validator_with_uploads.validate_oldest_file_in_proper_age_range()
         assert len(record) == 0
 
     @pytest.mark.slowtest
-    def test_validate_matt_server_backups_downloads_files(self, fresh_backup_bucket_with_downloads):
+    def test_validate_matt_server_backups_downloads_files(self, fresh_backup_validator_with_downloads):
         expected = ["mid1.txt", "mid2.txt", "mid3.txt", "mid4.txt"]
-        vb.validate_matt_server_backups_bucket(fresh_backup_bucket_with_downloads)
+        fresh_backup_validator_with_downloads.validate_matt_server_backups_bucket()
         actual_files = os.listdir(matt_server_backups_download_dir)
         assert expected == actual_files
--- validatebackups/validatebackups.py
+++ validatebackups/validatebackups.py
@@ -16,14 +16,6 @@ from warnings import warn
 from google.cloud import storage
 from tzlocal import get_localzone
 
-OLDEST_SERVER_BACKUP_FILE_MAX_AGE_IN_DAYS = 60
-NEWEST_SERVER_BACKUP_FILE_MAX_AGE_IN_DAYS = 7
-NUM_SERVER_BACKUPS_TO_DOWNLOAD = 4
-FILE_DOWNLOAD_LOCATION = os.path.join(r"D:\temp\backup_test")
-NUM_MEDIA_FILES_TO_DOWNLOAD = 5
-NUM_PHOTOS_FROM_THIS_MONTH_TO_DOWNLOAD = 5
-NUM_PHOTOS_FROM_EACH_YEAR_TO_DOWNLOAD = 10
-
 
 def main():
     """
@@ -47,161 +39,168 @@ def parse_args(args):
 
 def validate_backups():
     os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = r"D:\Matt\Documents\google cloud storage\backup-validator-auth.json"
-    client = storage.Client()
-
-    for bucket in client.list_buckets():
-        validate_bucket(bucket)
+    backup_validator = BackupValidator(client=storage.Client())
+    backup_validator.validate_backups()
 
 
-def validate_bucket(bucket):
-    if bucket.name == "giltaji-media":
-        validate_giltaji_media_bucket(bucket)
-    elif bucket.name == "giltaji-photos":
-        validate_giltaji_photos_bucket(bucket)
-    elif bucket.name == "matt-server-backups":
-        validate_matt_server_backups_bucket(bucket)
-    elif bucket.name == "giltajidjangobackups":
+class BackupValidator:
+    FILE_DOWNLOAD_LOCATION = os.path.join(r"D:\temp\backup_test")
+    OLDEST_SERVER_BACKUP_FILE_MAX_AGE_IN_DAYS = 60
+    NEWEST_SERVER_BACKUP_FILE_MAX_AGE_IN_DAYS = 7
+    NUM_SERVER_BACKUPS_TO_DOWNLOAD = 4
+    NUM_MEDIA_FILES_TO_DOWNLOAD = 3
+    NUM_PHOTOS_FROM_THIS_MONTH_TO_DOWNLOAD = 5
+    NUM_PHOTOS_FROM_EACH_YEAR_TO_DOWNLOAD = 10
+
+    def __init__(self, client=None, media_bucket=None, photo_bucket=None, server_backups_bucket=None):
+        self.client = client
+        self.media_bucket = media_bucket
+        self.photos_bucket = photo_bucket
+        self.server_backups_bucket = server_backups_bucket
+
+    def validate_backups(self):
+        for bucket in self.client.list_buckets():
+            self.validate_bucket(bucket)
+
+    def validate_bucket(self, bucket):
+        if bucket.name == "giltaji-media":
+            self.media_bucket = bucket
+            self.validate_giltaji_media_bucket()
+        elif bucket.name == "giltaji-photos":
+            self.photos_bucket = bucket
+            self.validate_giltaji_photos_bucket()
+        elif bucket.name == "matt-server-backups":
+            self.server_backups_bucket = bucket
+            self.validate_matt_server_backups_bucket()
+        elif bucket.name == "giltajidjangobackups":
+            pass
+        else:
+            raise ValueError("Unable to handle Bucket " + bucket.name)
+
+    def validate_giltaji_media_bucket(self):
+        folders = self.get_top_level_folders()
+        self.download_random_media_files_from_each_folder(folders)
+
+    def get_top_level_folders(self):
+        return []
+
+    def download_random_media_files_from_each_folder(self, folders):
+        for folder in folders:
+            self.download_random_media_files_from_folder(folder)
+
+    def download_random_media_files_from_folder(self, folder):
         pass
-    else:
-        raise ValueError("Unable to handle Bucket " + bucket.name)
+        #  select random sample of files -> NUM_MEDIA_FILES_TO_DOWNLOAD
+        #  download those files
 
+    @staticmethod
+    def get_random_sample_of_blobs(bucket, filter_criteria, sample_size):
+        filtered_blobs = bucket.list_blobs(prefix=filter_criteria)
+        return random.sample(population=filtered_blobs, k=sample_size)
 
-def validate_giltaji_media_bucket(bucket):
-    folders = get_top_level_folders(bucket)
-    download_random_media_files_from_each_folder(bucket, folders)
+    @classmethod
+    def download_blobs(cls, blobs):
+        for blob in blobs:
+            cls.download_blob(blob)
 
+    @classmethod
+    def download_blob(cls, blob):
+        file_location = cls.get_download_location_for_blob(blob)
+        BackupValidator.create_missing_directories(file_location)
+        blob.download_to_filename(file_location)
 
-def get_top_level_folders(bucket):
-    return []
+    @staticmethod
+    def create_missing_directories(file_path):
+        dir_only_file_path = os.path.dirname(file_path)
+        try:
+            os.makedirs(dir_only_file_path, exist_ok=True)
+        except OSError as exception:
+            if exception.errno != errno.EEXIST:
+                raise
 
+    @classmethod
+    def get_download_location_for_blob(cls, blob):
+        bucket_name = blob.bucket.name
+        blob_name = blob.name
+        return os.path.join(cls.FILE_DOWNLOAD_LOCATION, bucket_name, blob_name)
 
-def download_random_media_files_from_each_folder(bucket, folders):
-    for folder in folders:
-        download_random_media_files_from_folder(bucket, folder)
+    def validate_giltaji_photos_bucket(self):
+        self.download_random_photos_from_this_month()
+        self.download_random_photos_from_each_year()
 
+    def download_random_photos_from_this_month(self):
+        current_year = datetime.now().year
+        current_month = datetime.now().month
+        blobs = BackupValidator.get_random_sample_of_blobs(
+            bucket=self.photos_bucket,
+            filter_criteria=str(current_year) + "-" + str(current_month),
+            sample_size=self.NUM_PHOTOS_FROM_THIS_MONTH_TO_DOWNLOAD
+        )
+        self.download_blobs(blobs)
 
-def download_random_media_files_from_folder(bucket, folder):
-    pass
-    #  select random sample of files -> NUM_MEDIA_FILES_TO_DOWNLOAD
-    #  download those files
+    def download_random_photos_from_each_year(self):
+        current_year = datetime.now().year
+        for year in range(2010, current_year+1):
+            self.download_random_photos_from_specific_year(year)
 
+    def download_random_photos_from_specific_year(self, year):
+        blobs = BackupValidator.get_random_sample_of_blobs(
+            bucket=self.photos_bucket,
+            filter_criteria=str(year) + "-",
+            sample_size=self.NUM_PHOTOS_FROM_EACH_YEAR_TO_DOWNLOAD
+        )
+        self.download_blobs(blobs)
 
-def validate_giltaji_photos_bucket(bucket):
-    download_random_photos_from_this_month(bucket)
-    download_random_photos_from_each_year(bucket)
+    def validate_matt_server_backups_bucket(self):
+        self.validate_newest_file_in_proper_age_range()
+        self.validate_oldest_file_in_proper_age_range()
+        self.download_most_recent_server_backups()
 
+    def validate_newest_file_in_proper_age_range(self):
+        newest_blob = BackupValidator.get_newest_blob(self.server_backups_bucket.list_blobs())
+        if BackupValidator.get_blob_age_in_days(newest_blob) > self.NEWEST_SERVER_BACKUP_FILE_MAX_AGE_IN_DAYS:
+            warn("The newest file, " + newest_blob.name + ", is more than " +
+                 str(self.NEWEST_SERVER_BACKUP_FILE_MAX_AGE_IN_DAYS) +
+                 "days old! Check matt-server-backup cron job.")
 
-def download_random_photos_from_this_month(bucket):
-    current_year = datetime.now().year
-    current_month = datetime.now().month
-    blobs = get_random_sample_of_blobs(
-        bucket=bucket,
-        filter_criteria=str(current_year) + "-" + str(current_month),
-        sample_size=NUM_PHOTOS_FROM_THIS_MONTH_TO_DOWNLOAD
-    )
-    download_blobs(blobs)
+    @staticmethod
+    def get_newest_blob(blobs):
+        sorted_blobs = BackupValidator.get_blobs_sorted_newest_to_oldest(blobs)
+        return sorted_blobs[0]
 
+    @staticmethod
+    def get_blobs_sorted_newest_to_oldest(blobs):
+        return sorted(blobs, key=operator.attrgetter("time_created"), reverse=True)
 
-def download_blobs(blobs):
-    for blob in blobs:
-        download_blob(blob)
+    @staticmethod
+    def get_blob_age_in_days(blob):
+        now = BackupValidator.get_localized_time(datetime.now())
+        age = now - blob.time_created
+        return age.days
 
+    @staticmethod
+    def get_localized_time(timestamp):
+        return get_localzone().localize(timestamp)
 
-def download_blob(blob):
-    file_location = get_download_location_for_blob(blob)
-    create_missing_directories(file_location)
-    blob.download_to_filename(file_location)
+    def validate_oldest_file_in_proper_age_range(self):
+        oldest_blob = BackupValidator.get_oldest_blob(self.server_backups_bucket.list_blobs())
+        if BackupValidator.get_blob_age_in_days(oldest_blob) > self.OLDEST_SERVER_BACKUP_FILE_MAX_AGE_IN_DAYS:
+            warn("The oldest file, " + oldest_blob.name + ", is more than " +
+                 str(self.OLDEST_SERVER_BACKUP_FILE_MAX_AGE_IN_DAYS) +
+                 "days old! Check matt-server-backup lifecycle delete rules.")
 
+    @staticmethod
+    def get_oldest_blob(blobs):
+        sorted_blobs = BackupValidator.get_blobs_sorted_newest_to_oldest(blobs)
+        return sorted_blobs[-1]
 
-def get_download_location_for_blob(blob):
-    bucket_name = blob.bucket.name
-    blob_name = blob.name
-    return os.path.join(FILE_DOWNLOAD_LOCATION, bucket_name, blob_name)
+    def download_most_recent_server_backups(self):
+        blobs_to_download = self.get_most_recent_backup_blobs_to_download(self.server_backups_bucket.list_blobs())
+        self.download_blobs(blobs_to_download)
 
-
-def create_missing_directories(file_path):
-    dir_only_file_path = os.path.dirname(file_path)
-    try:
-        os.makedirs(dir_only_file_path, exist_ok=True)
-    except OSError as exception:
-        if exception.errno != errno.EEXIST:
-            raise
-
-
-def get_random_sample_of_blobs(bucket, filter_criteria, sample_size):
-    filtered_blobs = bucket.list_blobs(prefix=filter_criteria)
-    return random.sample(population=filtered_blobs, k=sample_size)
-
-
-def download_random_photos_from_each_year(bucket):
-    current_year = datetime.now().year
-    for year in range(2010, current_year+1):
-        download_random_photos_from_specific_year(year, bucket)
-
-
-def download_random_photos_from_specific_year(year, bucket):
-    blobs = get_random_sample_of_blobs(
-        bucket=bucket,
-        filter_criteria=str(year) + "-",
-        sample_size=NUM_PHOTOS_FROM_EACH_YEAR_TO_DOWNLOAD
-    )
-    download_blobs(blobs)
-
-
-def validate_matt_server_backups_bucket(bucket):
-    validate_newest_file_in_proper_age_range(bucket)
-    validate_oldest_file_in_proper_age_range(bucket)
-    download_most_recent_files(bucket)
-
-
-def validate_newest_file_in_proper_age_range(bucket):
-    newest_blob = get_newest_blob(bucket.list_blobs())
-    if get_blob_age_in_days(newest_blob) > NEWEST_SERVER_BACKUP_FILE_MAX_AGE_IN_DAYS:
-        warn("The newest file, " + newest_blob.name + ", is more than " +
-             str(NEWEST_SERVER_BACKUP_FILE_MAX_AGE_IN_DAYS) +
-             "days old! Check matt-server-backup cron job.")
-
-
-def get_newest_blob(blobs):
-    sorted_blobs = get_blobs_sorted_newest_to_oldest(blobs)
-    return sorted_blobs[0]
-
-
-def get_blobs_sorted_newest_to_oldest(blobs):
-    return sorted(blobs, key=operator.attrgetter("time_created"), reverse=True)
-
-
-def get_blob_age_in_days(blob):
-    now = get_localized_time(datetime.now())
-    age = now - blob.time_created
-    return age.days
-
-
-def get_localized_time(timestamp):
-    return get_localzone().localize(timestamp)
-
-
-def validate_oldest_file_in_proper_age_range(bucket):
-    oldest_blob = get_oldest_blob(bucket.list_blobs())
-    if get_blob_age_in_days(oldest_blob) > OLDEST_SERVER_BACKUP_FILE_MAX_AGE_IN_DAYS:
-        warn("The oldest file, " + oldest_blob.name + ", is more than " +
-             str(OLDEST_SERVER_BACKUP_FILE_MAX_AGE_IN_DAYS) +
-             "days old! Check matt-server-backup lifecycle delete rules.")
-
-
-def get_oldest_blob(blobs):
-    sorted_blobs = get_blobs_sorted_newest_to_oldest(blobs)
-    return sorted_blobs[-1]
-
-
-def download_most_recent_files(bucket):
-    blobs_to_download = get_most_recent_blobs_to_download(bucket.list_blobs())
-    download_blobs(blobs_to_download)
-
-
-def get_most_recent_blobs_to_download(blobs):
-    sorted_blobs = get_blobs_sorted_newest_to_oldest(blobs)
-    return sorted_blobs[:NUM_SERVER_BACKUPS_TO_DOWNLOAD]
+    def get_most_recent_backup_blobs_to_download(self, blobs):
+        sorted_blobs = BackupValidator.get_blobs_sorted_newest_to_oldest(blobs)
+        return sorted_blobs[:self.NUM_SERVER_BACKUPS_TO_DOWNLOAD]
 
 
 if __name__ == "__main__":  # pragma: no cover
