a116edde88efdd30c6f6c8142d0422150c7744cf
Matt Giltaji <matt@giltaji.com>
2020-02-23 20:15:33 -0800

    another folder to sync to gcs

--- sync_pictures_to_google_cloud.ps1
+++ sync_pictures_to_google_cloud.ps1
@@ -1,5 +1,6 @@
 # do the sync
 gsutil -m cp -n -r D:\Matt\Pictures\20* gs://giltaji-photos/
+gsutil -m cp -n -r D:\Matt\Pictures\specia* gs://giltaji-photos/
 
 # let pushmon know we succeeded
 $urlString = "http://pshmn.com/WMgnwq"
