a3dfefb6632442cd24812337b11f221159ed2a55
Matt Giltaji <matt@giltaji.com>
2026-06-24 20:08:39 -0700

    update gsutil to gcloud

--- sync_pictures_to_google_cloud.ps1
+++ sync_pictures_to_google_cloud.ps1
@@ -1,7 +1,8 @@
 # do the sync
-gsutil -m cp -n -r D:\Matt\Pictures\202* gs://giltaji-photos/
-gsutil -m cp -n -r D:\Matt\Pictures\blase* gs://giltaji-misc/
-gsutil -m cp -n -r D:\Matt\Pictures\specia* gs://giltaji-photos/
+gcloud storage cp -n -r D:\Matt\OneDrive\Pictures\202* gs://giltaji-photos/
+gcloud storage cp -n -r D:\Matt\OneDrive\Pictures\blase* gs://giltaji-misc/
+gcloud storage cp -n -r D:\Matt\OneDrive\Pictures\specia* gs://giltaji-photos/
+gcloud storage cp -n -r D:\Matt\OneDrive\Pictures\Deva* gs://giltaji-photos/
 
 # let pushmon know we succeeded
 $urlString = "http://pshmn.com/WMgnwq"
